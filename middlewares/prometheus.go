3a2db2c145ce5073bb25c4f606f177dc3eeb0a51
antoine.leveugle <antoine.leveugle@gmail.com>
2019-04-05 10:50:53 +0200

    fix(prometheus): change label names and use default buckets

--- middlewares/prometheus.go
+++ middlewares/prometheus.go
@@ -9,49 +9,34 @@ import (
 )
 
 var (
-	dflBuckets = []float64{300, 1200, 5000}
-	labelNames = []string{"code", "method", "path"}
+	labelNames = []string{"status", "method", "uri"}
 )
 
 const (
-	reqsName    = "http_requests_total"
-	latencyName = "http_request_duration_milliseconds"
+	latencyName = "http_server_requests_seconds"
 )
 
 type PrometheusMiddleware struct {
-	reqs    *prometheus.CounterVec
 	latency *prometheus.HistogramVec
 }
 
-func NewPrometheusMiddleware(name string, buckets ...float64) *PrometheusMiddleware {
+func NewPrometheusMiddleware(name string) *PrometheusMiddleware {
 	labels := prometheus.Labels{"service": name}
 
-	var m PrometheusMiddleware
-	m.reqs = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name:        reqsName,
-			Help:        "How many HTTP requests processed, partitioned by status code, method and HTTP path.",
-			ConstLabels: labels,
-		},
-		labelNames,
-	)
-	prometheus.MustRegister(m.reqs)
-
-	if len(buckets) == 0 {
-		buckets = dflBuckets
+	m := &PrometheusMiddleware{
+		latency: prometheus.NewHistogramVec(
+			prometheus.HistogramOpts{
+				Name:        latencyName,
+				Help:        "How long it took to process the request, partitioned by status code, method and HTTP path.",
+				ConstLabels: labels,
+			},
+			labelNames,
+		),
 	}
-	m.latency = prometheus.NewHistogramVec(
-		prometheus.HistogramOpts{
-			Name:        latencyName,
-			Help:        "How long it took to process the request, partitioned by status code, method and HTTP path.",
-			ConstLabels: labels,
-			Buckets:     buckets,
-		},
-		labelNames,
-	)
+
 	prometheus.MustRegister(m.latency)
 
-	return &m
+	return m
 }
 
 func (m *PrometheusMiddleware) Handler() gin.HandlerFunc {
@@ -60,7 +45,6 @@ func (m *PrometheusMiddleware) Handler() gin.HandlerFunc {
 
 		c.Next()
 
-		m.reqs.WithLabelValues(strconv.Itoa(c.Writer.Status()), c.Request.Method, c.Request.URL.Path).Inc()
-		m.latency.WithLabelValues(strconv.Itoa(c.Writer.Status()), c.Request.Method, c.Request.URL.Path).Observe(float64(time.Since(start).Nanoseconds()) / 1000000)
+		m.latency.WithLabelValues(strconv.Itoa(c.Writer.Status()), c.Request.Method, c.Request.URL.Path).Observe(time.Since(start).Seconds())
 	}
 }
