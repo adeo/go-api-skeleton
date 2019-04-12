4dce54e6a07091017782ac97813f13ba80f7c20e
Loic DASSONVILLE <ldassonville@PO-0289.local>
2019-04-12 16:21:43 +0200

    fix(sed): Allow mac support

--- duplicate.sh
+++ duplicate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+command -v gsed > /dev/null 2>&1 /dev/null && SED_CMD=gsed || SED_CMD=sed
 
 OLD_PROJECT_NAMESPACE="github.com/adeo"
 OLD_PROJECT_NAME="turbine-go-api-skeleton"
@@ -14,14 +14,14 @@ echo "What is the new project name? (Eg. if you are creating project under 'gith
 read NEW_PROJECT_NAME
 
 NEW_PROJECT_FULL_NAME="${NEW_PROJECT_NAMESPACE}/${NEW_PROJECT_NAME}"
-OLD_PROJECT_GIT="$(echo "$OLD_PROJECT_NAMESPACE" | sed 's|/|:|')"
-NEW_PROJECT_GIT="$(echo "$NEW_PROJECT_NAMESPACE" | sed 's|/|:|')"
+OLD_PROJECT_GIT="$(echo "$OLD_PROJECT_NAMESPACE" | $SED_CMD 's|/|:|')"
+NEW_PROJECT_GIT="$(echo "$NEW_PROJECT_NAMESPACE" | $SED_CMD 's|/|:|')"
 
-find . -iname '*.go' -exec sed -i "s|${OLD_PROJECT_FULL_NAME}|${NEW_PROJECT_FULL_NAME}|g" {} \;
-sed -i "s|${OLD_PROJECT_FULL_NAME}|${NEW_PROJECT_FULL_NAME}|g" Makefile Dockerfile go.mod
-sed -i "s|${OLD_PROJECT_NAME}|${NEW_PROJECT_NAME}|g" Makefile Dockerfile info.yaml cmd/root.go tom.yml .gitlab-ci.yml
-sed -i "s|${OLD_PROJECT_GROUP}|${NEW_PROJECT_GROUP}|g" Makefile Dockerfile tom.yml .gitlab-ci.yml
-sed -i "s|${OLD_PROJECT_GIT}|${NEW_PROJECT_GIT}|g" .gitlab-ci.yml
+find . -iname '*.go' -exec $SED_CMD -i "s|${OLD_PROJECT_FULL_NAME}|${NEW_PROJECT_FULL_NAME}|g" {} \;
+$SED_CMD -i "s|${OLD_PROJECT_FULL_NAME}|${NEW_PROJECT_FULL_NAME}|g" Makefile Dockerfile go.mod
+$SED_CMD -i "s|${OLD_PROJECT_NAME}|${NEW_PROJECT_NAME}|g" Makefile Dockerfile info.yaml cmd/root.go tom.yml .gitlab-ci.yml
+$SED_CMD -i "s|${OLD_PROJECT_GROUP}|${NEW_PROJECT_GROUP}|g" Makefile Dockerfile tom.yml .gitlab-ci.yml
+$SED_CMD -i "s|${OLD_PROJECT_GIT}|${NEW_PROJECT_GIT}|g" .gitlab-ci.yml
 
 main()
 {
@@ -32,42 +32,42 @@ main()
     while true
     do
         echo "Creating a new entity:"
-        echo "What is the entity name you want to create? (name to be used in URL path, write it lower case, singular)"
+        echo "What is the entity name you want to create? (name to be u$SED_CMD in URL path, write it lower case, singular)"
         read ENTITY_NAME
-        ENTITY_NAME_UP="${ENTITY_NAME^}"
+        ENTITY_NAME_UP=$(echo $ENTITY_NAME | tr  '[:upper:]' '[:lower:]')
 
         echo "What is the postgresql schema to use for this entity? (if you plan to use MongoDB you can ignore this question)"
         read ENTITY_SCHEMA
 
         cp handlers/template_handler.go handlers/${ENTITY_NAME}_handler.go
-        sed -i -r "s/template/${ENTITY_NAME}/g" handlers/${ENTITY_NAME}_handler.go
-        sed -i -r "s/Template/${ENTITY_NAME_UP}/g" handlers/${ENTITY_NAME}_handler.go
+        $SED_CMD -i -r "s/template/${ENTITY_NAME}/g" handlers/${ENTITY_NAME}_handler.go
+        $SED_CMD -i -r "s/Template/${ENTITY_NAME_UP}/g" handlers/${ENTITY_NAME}_handler.go
 
         cp storage/dao/postgresql/database_postgresql_template.go storage/dao/postgresql/database_postgresql_${ENTITY_NAME}.go
-        sed -i -r "s/template/${ENTITY_NAME}/g" storage/dao/postgresql/database_postgresql_${ENTITY_NAME}.go
-        sed -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/postgresql/database_postgresql_${ENTITY_NAME}.go
-        sed -i -r "s/schema/${ENTITY_SCHEMA}/g" storage/dao/postgresql/database_postgresql_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/template/${ENTITY_NAME}/g" storage/dao/postgresql/database_postgresql_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/postgresql/database_postgresql_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/schema/${ENTITY_SCHEMA}/g" storage/dao/postgresql/database_postgresql_${ENTITY_NAME}.go
 
         cp storage/dao/mongodb/database_mongodb_template.go storage/dao/mongodb/database_mongodb_${ENTITY_NAME}.go
-        sed -i -r "s/template/${ENTITY_NAME}/g" storage/dao/mongodb/database_mongodb_${ENTITY_NAME}.go
-        sed -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/mongodb/database_mongodb_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/template/${ENTITY_NAME}/g" storage/dao/mongodb/database_mongodb_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/mongodb/database_mongodb_${ENTITY_NAME}.go
 
         cp storage/dao/mock/database_mock_template.go storage/dao/mock/database_mock_${ENTITY_NAME}.go
-        sed -i -r "s/template/${ENTITY_NAME}/g" storage/dao/mock/database_mock_${ENTITY_NAME}.go
-        sed -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/mock/database_mock_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/template/${ENTITY_NAME}/g" storage/dao/mock/database_mock_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/mock/database_mock_${ENTITY_NAME}.go
 
         cp storage/dao/fake/database_fake_template.go storage/dao/fake/database_fake_${ENTITY_NAME}.go
-        sed -i -r "s/template/${ENTITY_NAME}/g" storage/dao/fake/database_fake_${ENTITY_NAME}.go
-        sed -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/fake/database_fake_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/template/${ENTITY_NAME}/g" storage/dao/fake/database_fake_${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/dao/fake/database_fake_${ENTITY_NAME}.go
 
         cp storage/model/template.go storage/model/${ENTITY_NAME}.go
-        sed -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/model/${ENTITY_NAME}.go
+        $SED_CMD -i -r "s/Template/${ENTITY_NAME_UP}/g" storage/model/${ENTITY_NAME}.go
 
-        sed -i -r "/\/\/ start: template routes/{:next;N;/\/\/ end: template routes/{bend};bnext;:end;p;s|template|${ENTITY_NAME}|g;s|Template|${ENTITY_NAME_UP}|g}" handlers/handler.go
+        $SED_CMD -i -r "/\/\/ start: template routes/{:next;N;/\/\/ end: template routes/{bend};bnext;:end;p;s|template|${ENTITY_NAME}|g;s|Template|${ENTITY_NAME_UP}|g}" handlers/handler.go
 
-        sed -i -r "/\/\/ start: template dao funcs/{:next;N;/\/\/ end: template dao funcs/{bend};bnext;:end;p;s|template|${ENTITY_NAME}|g;s|Template|${ENTITY_NAME_UP}|g}" storage/dao/database.go
+        $SED_CMD -i -r "/\/\/ start: template dao funcs/{:next;N;/\/\/ end: template dao funcs/{bend};bnext;:end;p;s|template|${ENTITY_NAME}|g;s|Template|${ENTITY_NAME_UP}|g}" storage/dao/database.go
 
-        sed -i -r "/\/\/ template export/{p;s/Template/${ENTITY_NAME}/g}" storage/dao/fake/database_fake.go
+        $SED_CMD -i -r "/\/\/ template export/{p;s/Template/${ENTITY_NAME}/g}" storage/dao/fake/database_fake.go
 
         echo "Done"
         echo "If you want to stop here, hit Ctrl+C"
